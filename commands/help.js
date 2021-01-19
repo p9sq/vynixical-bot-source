b48be3b01b699faf0d2562e0a3b27d205ad636fe
p9sq <62570697+p9sq@users.noreply.github.com>
2021-01-19 17:29:43 +1300

    update

--- commands/help.js
+++ commands/help.js
@@ -7,12 +7,9 @@ module.exports.run = async (bot, message, args) => {
     if(!args[0]) {
         let embed = new Discord.MessageEmbed()
         guildprefix.findOne({guildID: message.guild.id, guildName: message.guild.name, guildOwner: message.guild.owner.user.tag}, (err, data) => {
-            embed.setAuthor(message.guild.name, message.guild.iconURL({dynamic: true}))
-            embed.setTitle(`${bot.user.username}'s available commands!`)
+            embed.setTitle(`${bot.user.username} Help Menu`)
             embed.setColor(color)
-            embed.setThumbnail(`${bot.user.avatarURL({size: 2048, format: "png"})}`)
             embed.setDescription(`[Add bot to server](https://discord.com/api/oauth2/authorize?client_id=725582436477698118&permissions=8&scope=bot), [Support Server](https://discord.gg/sr2JWV6)`)
-            // bot.utils.insertCommands(bot, embed)
 	        if(!owners.includes(message.author.id)) {
 		        categories = bot.utils.removeDuplicates(bot.commands.filter(cmd => cmd.config.category !== "Developer").map(cmd => cmd.config.category));
 	        } else {
@@ -22,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
 	        for (const category of categories) {
 		        embed.addField(category, bot.commands.filter(cmd => cmd.config.category === category).map(cmd => `\`${cmd.config.name}\``).join(", "));
 	        }
-            embed.setFooter(`${bot.user.username} | For more help, do ${data.prefix}help [command]`, bot.user.displayAvatarURL({format: "png"}))
+            embed.setFooter(`For more help, do ${data.prefix}help [command]`)
             embed.setTimestamp()
         message.channel.send(embed)
         })
