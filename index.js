ee2a2bda59d3710c6babfd15d141e98a1e906da0
p9sq <62570697+p9sq@users.noreply.github.com>
2021-01-19 20:39:47 +1300

    update

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ bot.on("ready", async () => {
   loadCommands(bot);
   loadEvents(bot);
   const statuses = [
-    `${bot.guilds.cache.size.toLocaleString()} servers | >help`,
-    `${bot.channels.cache.size.toLocaleString()} channels | >help`,
-    `${bot.users.cache.size.toLocaleString()} users | >help`,
-    `${bot.emojis.cache.size.toLocaleString()} emojis | >help`,
-    `${bot.ws.totalShards} shard(s) | >help`
+    `Connected to ${bot.guilds.cache.size.toLocaleString()} servers | ??help`,
+    `Watching ${bot.channels.cache.size.toLocaleString()} channels | ??help`,
+    `Listening to ${bot.users.cache.size.toLocaleString()} users | ??help`,
+    `Counting ${bot.emojis.cache.size.toLocaleString()} emojis | ??help`,
+    `Looking at ${bot.ws.totalShards} shard(s) | >help`
   ];
   const status = statuses[Math.floor(Math.random() * statuses.length)];
-  bot.user.setPresence({activity: {name: status, type: "WATCHING"}, status: "online"})
-  setInterval(() => bot.user.setPresence({activity: {name: status, type: "WATCHING"}, status: "online"}), 60000);
+  bot.user.setPresence({activity: {name: status, type: "PLAYING"}, status: "idle"});
+  setInterval(() => bot.user.setPresence({activity: {name: status, type: "PLAYING"}, status: "idle"}), 60000);
   setInterval(() => require("./ibl-poststats")(bot), 3e5);
   mongoose.connect("mongodb+srv://admin:pz1234567@snakeboy-uoihx.mongodb.net/SnakeBoy_events?retryWrites=true&w=majority", {useNewUrlParser: true,
   useUnifiedTopology: true}).then(console.log(`[${utc().format("HH:mm:ss")}] Successfully connected to MongoDB`));
