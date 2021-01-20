4ae15df1aa7ed6a6882d26c2791fe6a564bf0b3b
p9sq <62570697+p9sq@users.noreply.github.com>
2021-01-20 20:39:46 +1300

    update

---
+++ models/bot.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema({
+    banner: String,
+    changeLogs: {
+        changeLog: String,
+        date: String,
+        version: String
+    }
+});
+module.exports = mongoose.model("bot", Schema);
