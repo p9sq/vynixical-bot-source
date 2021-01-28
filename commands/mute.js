535fa09dc3602f4c79cf4cbdf00e71a792aef31d
p9sq <62570697+p9sq@users.noreply.github.com>
2021-01-28 15:52:36 +1300

    update

--- commands/mute.js
+++ commands/mute.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const mutedRole = require("../models/mute");
 const modlogs = require("../models/logchannel");
-const cases = require("../models/cases");
 
 module.exports.run = async (bot, message, args) => {
     const Member = message.mentions.users.last();
@@ -22,28 +21,12 @@ module.exports.run = async (bot, message, args) => {
             member.roles.add(m.muteID);
             message.channel.send(`<:check:314349398811475968> Successfully muted **${Member.tag}** with reason **${reason}**!`)
 
-            cases.findOne({guildID: message.guild.id}, (err, data) => {
-              if(!data) {
-                const newData = cases({
-                  guildID: message.guild.id,
-                  cases: [
-                    {
-                      id: 1,
-                      action: "mute",
-                      moderator: message.author.id,
-                      member: member.user.id,
-                      reason: reason
-                    }
-                  ]
-                });
-                newData.save();
-                const muteEmbed = new Discord.MessageEmbed()
+            const muteEmbed = new Discord.MessageEmbed()
               modlogs.findOne({ guildID: message.guild.id}, (err , ch) => {
                 muteEmbed.setAuthor(`[MUTE] ${Member.tag}`, `${Member.displayAvatarURL()}`)
                 muteEmbed.addField("Server muted in:", `${message.guild.name}\n\`(${message.guild.id})\``, true)
                 muteEmbed.addField("Moderator:", `${message.author.tag}\n\`(${message.author.id})\``, true)
                 muteEmbed.addField("Reason:", `${reason}`, true)
-                muteEmbed.addField("Case No.:", newData.cases.id, true)
                 if(!ch) {
                   return;
                 } else {
@@ -51,31 +34,6 @@ module.exports.run = async (bot, message, args) => {
                   logschannel.send(muteEmbed)
                 }
               })
-              } else {
-                data.cases.unshift({
-                  id: data.cases.id- + -1,
-                  action: "mute",
-                  moderator: message.author.id,
-                  member: member.user.id,
-                  reason: reason
-                });
-                data.save();
-                const muteEmbed = new Discord.MessageEmbed()
-              modlogs.findOne({ guildID: message.guild.id}, (err , ch) => {
-                muteEmbed.setAuthor(`[MUTE] ${Member.tag}`, `${Member.displayAvatarURL()}`)
-                muteEmbed.addField("Server muted in:", `${message.guild.name}\n\`(${message.guild.id})\``, true)
-                muteEmbed.addField("Moderator:", `${message.author.tag}\n\`(${message.author.id})\``, true)
-                muteEmbed.addField("Reason:", `${reason}`, true)
-                muteEmbed.addField("Case No.:", data.cases.id, true)
-                if(!ch) {
-                  return;
-                } else {
-                  const logschannel = message.guild.channels.cache.get(ch.channelID)
-                  logschannel.send(muteEmbed)
-                }
-              })
-              }
-            })
       })
       }
 }
--- commands/unmute.js
+++ commands/unmute.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const mutedRole = require("../models/mute");
 const modlogs = require("../models/logchannel");
-const cases = require("../models/cases");
 
 module.exports.run = async (bot, message, args) => {
     const Member = message.mentions.users.last();
@@ -19,32 +18,15 @@ module.exports.run = async (bot, message, args) => {
         if(!reason) reason = "No reason provided"
         mutedRole.findOne({ guildID: message.guild.id } , ( err , m ) => {
           if(!m) return;
-            member.roles.remove(m.muteID);
+            member.roles.add(m.muteID);
             message.channel.send(`<:check:314349398811475968> Successfully unmuted **${Member.tag}** with reason **${reason}**!`)
 
-            cases.findOne({guildID: message.guild.id}, (err, data) => {
-              if(!data) {
-                const newData = cases({
-                  guildID: message.guild.id,
-                  cases: [
-                    {
-                      id: 1,
-                      action: "unmute",
-                      moderator: message.author.id,
-                      member: member.user.id,
-                      reason: reason
-                    }
-                  ]
-                });
-                newData.save();
-                newData.save();
-                const muteEmbed = new Discord.MessageEmbed()
+            const muteEmbed = new Discord.MessageEmbed()
               modlogs.findOne({ guildID: message.guild.id}, (err , ch) => {
                 muteEmbed.setAuthor(`[UNMUTE] ${Member.tag}`, `${Member.displayAvatarURL()}`)
                 muteEmbed.addField("Server unmuted in:", `${message.guild.name}\n\`(${message.guild.id})\``, true)
                 muteEmbed.addField("Moderator:", `${message.author.tag}\n\`(${message.author.id})\``, true)
                 muteEmbed.addField("Reason:", `${reason}`, true)
-                muteEmbed.addField("Case No.:", `${newData.cases.caseID}`, true)
                 if(!ch) {
                   return;
                 } else {
@@ -52,31 +34,6 @@ module.exports.run = async (bot, message, args) => {
                   logschannel.send(muteEmbed)
                 }
               })
-              } else {
-                data.cases.unshift({
-                  id: data.cases.id++,
-                  action: "unmute",
-                  moderator: message.author.id,
-                  member: member.user.id,
-                  reason: reason
-                });
-                data.save();
-                const muteEmbed = new Discord.MessageEmbed()
-              modlogs.findOne({ guildID: message.guild.id}, (err , ch) => {
-                muteEmbed.setAuthor(`[UNMUTE] ${Member.tag}`, `${Member.displayAvatarURL()}`)
-                muteEmbed.addField("Server unmuted in:", `${message.guild.name}\n\`(${message.guild.id})\``, true)
-                muteEmbed.addField("Moderator:", `${message.author.tag}\n\`(${message.author.id})\``, true)
-                muteEmbed.addField("Reason:", `${reason}`, true)
-                muteEmbed.addField("Case No.:", `${data.cases.caseID}`, true)
-                if(!ch) {
-                  return;
-                } else {
-                  const logschannel = message.guild.channels.cache.get(ch.channelID)
-                  logschannel.send(muteEmbed)
-                }
-              })
-              }
-            })
       })
       }
 }
@@ -86,7 +43,7 @@ module.exports.config = {
     description: "Unmutes a mentioned user with a reason",
     usage: "unmute <user> [reason]",
     category: "Moderation",
-    example: "unmute @Wumpus#0001 Please don't flood discord chats",
+    example: "unmute @Wumpus#0001 Stop flooding discord chats",
     accessableby: "Admins",
     aliases: ["unmuteuser", "unmutemember"]
 }
