1bb54aab16108ed57753515c084a7012bd790c70
p9sq <62570697+p9sq@users.noreply.github.com>
2021-01-31 18:58:48 +1300

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.on("ready", async () => {
     `Listening to ${bot.users.cache.size.toLocaleString()} users | ${botconfig.defaultPrefix}help`,
     `Counting ${bot.emojis.cache.size.toLocaleString()} emojis | ${botconfig.defaultPrefix}help`,
     `Total shards: ${bot.shard.count} | ${botconfig.defaultPrefix}help`,
-    "Visit http://www.vynixical.com/"
+    `Visit http://www.vynixical.com/ | ${botconfig.defaultPrefix}help`
   ];
   const status = statuses[Math.floor(Math.random() * statuses.length)];
   bot.user.setPresence({activity: {name: status, type: "PLAYING"}, status: "idle"});
