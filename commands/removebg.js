b044628eb558547bf5b52473f2b88a73287a1f3b
p9sq <62570697+p9sq@users.noreply.github.com>
2021-02-03 15:46:30 +1300

    update

--- commands/removebg.js
+++ commands/removebg.js
@@ -1,33 +1,37 @@
-const Discord = require("discord.js");
 const request = require("request");
+const guildprefix = require("../models/prefix");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.channel.send("Please specify an image url.");
-    if(!args[0].startsWith("https://")) return message.channel.send("Please provide a full image url with `https://` at the beginning.");
-    const image = args[0].toString();
+  const image = message.attachments.first();
+
+  guildprefix.findOne({guildID: message.guild.id}, (err, prefix) => {
+    if(!image) return message.channel.send(`Please make sure that you are uploading an image to discord while using the **${prefix.prefix}removebg** command.`);
+
     request.post({
-        url: "https://api.remove.bg/v1.0/removebg",
-        formData: {
-            image_file: image,
-            size: "auto",
-        },
-        headers: {
-          "X-Api-Key": "BBy8wMri5wkXP5F9xLfu9BJf"
-        },
-        encoding: null
-      }, function(error, response, body) {
-        if(error) return message.channel.send(`Request failed: \`${error}\``);
-        if(response.statusCode != 200) return message.channel.send(`Error: \`${response.statusCode, body.toString("utf8")}\``);
-        message.channel.send(new Discord.MessageAttachment("no-bg.png", body));
+      url: "https://api.remove.bg/v1.0/removebg",
+      formData: {
+        image_url: image.proxyURL,
+        size: "auto",
+      },
+      headers: {
+        "X-Api-Key": "BBy8wMri5wkXP5F9xLfu9BJf"
+      },
+      encoding: null
+    }, function(error, response, body) {
+      message.channel.send("Processing...");
+      if(error) return message.channel.send(`Request failed: \`${error}\``);
+      if(response.statusCode != 200) return message.channel.send(`Error: \`${response.statusCode, body.toString("utf8")}\``);
+      message.reply("Done! The background has been successfully removed!", new Discord.MessageAttachment(body, "no-bg.png"));
     });
+});
 }
 
 module.exports.config = {
     name: "removebg",
     description: "Removes the background of an image",
-    usage: "removebg https://example-image-url.png",
+    usage: "removebg <attachment>",
     category: "Image",
-    example: "removebg https://cdn.discordapp.com/avatars/155149108183695360/19a5ee4114b47195fcecc6646f2380b1.png",
+    example: "removebg [attachment-here-(make-sure-it's-not-a-url]",
     accessableby: "Everyone",
     aliases: ["removebackground", "rbackground"]
 }
