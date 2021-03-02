b3d099e07f94eb0175bc48d39fb4a4011c7b5603
p9sq <62570697+p9sq@users.noreply.github.com>
2021-03-02 16:12:22 +1300

    update

--- commands/poststats.js
+++ commands/poststats.js
@@ -8,62 +8,68 @@ module.exports.run = async (bot, message, args) => {
   if (!botconfig.owners.includes(message.author.id)) {
     return;
   } else {
+    const dbl = new DBL(botconfig.apiTokens.topgg, bot);
+
     const msg = await message.channel.send(
       "<a:loading:393852367751086090> | Posting..."
     );
 
-    const dbl = new DBL(botconfig.apiTokens.topgg, bot);
+    try {
+      dbl.postStats(bot.guilds.cache.size, bot.shard.ids[0], bot.shard.count);
+      stats.postStats(bot.guilds.cache.size, bot.shard.count);
 
-    dbl.postStats(bot.guilds.cache.size, bot.shard.ids[0], bot.shard.count);
-    stats.postStats(bot.guilds.cache.size, bot.shard.count);
+      dbl.on("posted", () => {
+        console.log("Server count posted!");
+      });
 
-    dbl.on("posted", () => {
-      console.log("Server count posted!");
-    });
+      fetch("https://discord.bots.gg/api/v1/bots/725582436477698118/stats", {
+        method: "POST",
+        body: JSON.stringify({
+          guildCount: bot.guilds.cache.size,
+          shardCount: bot.shard.count,
+          shardId: bot.shard.ids[0],
+        }),
+        headers: {
+          Authorization: botconfig.apiTokens.dbots,
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
 
-    fetch("https://discord.bots.gg/api/v1/bots/725582436477698118/stats", {
-      method: "POST",
-      body: JSON.stringify({
-        guildCount: bot.guilds.cache.size,
-        shardCount: bot.shard.count,
-        shardId: bot.shard.ids[0],
-      }),
-      headers: {
-        Authorization: botconfig.apiTokens.dbots,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+      fetch("https://cyclonebots.xyz/api/auth/stats/725582436477698118", {
+        method: "POST",
+        headers: {
+          Authorization: botconfig.apiTokens.cyclone,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ server_count: bot.guilds.cache.size }),
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
 
-    fetch("https://cyclonebots.xyz/api/auth/stats/725582436477698118", {
-      method: "POST",
-      headers: {
-        Authorization: botconfig.apiTokens.cyclone,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ server_count: bot.guilds.cache.size }),
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+      fetch("https://fateslist.xyz/api/bots/725582436477698118/stats", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          api_token: botconfig.apiTokens.fates,
+          guild_count: bot.guilds.cache.size,
+          shard_count: bot.shard.count,
+        }),
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
 
-    fetch("https://fateslist.xyz/api/bots/725582436477698118/stats", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        api_token: botconfig.apiTokens.fates,
-        guild_count: bot.guilds.cache.size,
-        shard_count: bot.shard.count,
-      }),
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-
-    msg.edit(
-      "<:allow:793205689753010217> **Successfully posted stats to all bot lists**"
-    );
+      msg.edit(
+        "<:allow:793205689753010217> **Successfully posted stats to all bot lists**"
+      );
+    } catch (err) {
+      msg.edit(
+        `<:deny:793205689488900136> **Error when posting stats:**\n\`\`\`js\n${err}\n\`\`\``
+      );
+    }
   }
 };
 
