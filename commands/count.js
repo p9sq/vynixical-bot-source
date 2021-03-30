6a649b7316fe32612302b2b081474d553953f73a
p9sq <ohohXD9@gmail.com>
2021-03-30 19:34:57 +1300

    update

---
+++ commands/count.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const { color } = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+  const embed = new Discord.MessageEmbed()
+    .setDescription(`Intents (\`Presence Intent\` & \`Server Members Intent\`) haven't been whitelisted yet. So don't expect the information provided to be correct.`)
+    .setColor(color)
+    .addField("Statistics", `
+    **Users:** ${bot.users.cache.size.toLocaleString()}
+    **Channels:** ${bot.channels.cache.size.toLocaleString()}
+    **Servers:** ${bot.guilds.cache.size.toLocaleString()}
+    **Emojis:** ${bot.emojis.cache.size}
+    `);
+  message.reply(embed);
+};
+
+module.exports.config = {
+  name: "count",
+  description: "Shows the cached users, channels, servers and emojis",
+  usage: "count",
+  category: "Info",
+  example: "count",
+  accessableby: "Everyone",
+  aliases: [],
+};
