20288b54a0aec55d9aa8ae54aaf1678fcd376208
p9sq <ohohXD9@gmail.com>
2021-03-30 15:54:22 +1300

    Slash commands :0

--- index.js
+++ index.js
@@ -75,6 +75,50 @@ bot.on("ready", async () => {
   const current = new Date();
   const giveaways = await Giveaway.find({ endsOn: { $gt: current } });
   await scheduleGiveaways(bot, giveaways);
+
+  bot.api.applications(bot.user.id).commands.post({
+    data: {
+      name: "whendie",
+      description: "Tells the user when they are about to die",
+      options: [
+        {
+          name: "member",
+          description: "The member you are mentioning to see when they die",
+          type: 6,
+          required: false
+        }
+      ]
+    }
+  });
+
+  bot.ws.on("INTERACTION_CREATE", async (interaction) => {
+    const command = interaction.data.name.toLowerCase();
+    const args = interaction.data.options;
+
+    if(command === "whendie") {
+      const member = args.find(arg => args.name.toLowerCase() === "member").value;
+      const number = Math.floor(Math.random() * 99) * 1000; 
+      const embed = new Discord.MessageEmbed()
+        .setDescription(`${member.user.tag} is going to die in ${require("ms")(number, { long: true })}`)
+        .setColor("RANDOM")
+        .setFooter(`Requested by ${interaction.member.user.tag}`);
+
+      bot.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+          type: 4,
+          data: await createAPIMessage(interaction, embed)
+        }
+      });
+    }
+  });
 });
+
+async function createAPIMessage(interaction, content) {
+  const apiMessage = await Discord.APIMessage.create(bot.channels.resolve(interaction.channel_id), content)
+    .resolveData()
+    .resolveFiles();
+
+  return apiMessage;
+}
 
 bot.login(botconfig.token);
